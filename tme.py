1639e34f53ff8d0eaf772fa5a919444b9deaa0f1
young1112 <99293037+young1112@users.noreply.github.com>
2022-04-21 23:47:33 +0100

    Delete tme.py

--- tme.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('git pull')
-
-tme=platform.architecture()[0]
-if tme=="32bit":
-    print('Sorry Update Your Phone...')
-elif tme=="64bit":
-    __import__("bs").reg()
